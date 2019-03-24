9f0fc56c2f864cbdf47c9b9315738b61bf7d39bf
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2019-03-24 22:19:06 +0100

    added better main for preview and testing

--- code/main.cpp
+++ code/main.cpp
@@ -1,10 +1,18 @@
 #include "../headers/mp3_player.hpp"
+#include "hardware_usart.hpp"
 #include "hwlib.hpp"
 
 int main(void) {
     // kill the watchdog
     WDT->WDT_MR = WDT_MR_WDDIS;
     hwlib::wait_ms(1000);
+    hwlib::cout << "this works on arduino";
+    r2d2::uart_ports_c port = r2d2::uart_ports_c::uart1;
+    auto c = r2d2::hardware_usart_c(9600, port);
+    char cc;
+    char gps_buffer[100];
+    bool complete_string = false;
+    int n = 0;
     if (true) { // player is mp3 board
         auto power = hwlib::target::pin_out(hwlib::target::pins::d3);
         auto repeat = hwlib::target::pin_oc(hwlib::target::pins::d4);
@@ -50,5 +58,24 @@ int main(void) {
             }
         }
     } else { // player is DFPlayer
+
+        for (;;) {
+            if (c.char_available()) {
+                cc = c.getc();
+                if (cc == '$') {
+                    complete_string = true;
+                    n = 0;
+                    gps_buffer[n] = cc;
+                } else if (cc == '\n' || cc == '\r') {
+                    if (complete_string) {
+                        hwlib::cout << gps_buffer << "\n";
+                    }
+                    complete_string = false;
+                } else {
+                    n++;
+                    gps_buffer[n] = cc;
+                }
+            }
+        }
     }
 }
