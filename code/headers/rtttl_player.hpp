c2bcf2886c96ecc6ff121b58ab694d241bbef87a
DavidJong05 <36602654+DavidJong05@users.noreply.github.com>
2019-05-22 11:42:21 +0200

    Doxygen rtttl_player_c
    Doxygen added (not finished)

--- code/headers/rtttl_player.hpp
+++ code/headers/rtttl_player.hpp
@@ -3,13 +3,26 @@
 #include <note_player.hpp>
 
 namespace r2d2::sound {
-
+    /**
+     * class rtttl_player_c
+     * Provides hardware I2C functionality to the arduino due.
+     */
     class rtttl_player_c {
     private:
         note_player_c & player;
     public:
-
+        /**
+         * Constructor that makes player out of abstract class
+         *
+         * @param player - make player from note_player_c
+         */
         rtttl_player_c(note_player_c & player);
+        
+        /**
+         * Plays a song that is made from ceveral frequencies/notes
+         * 
+         * @param *s - ceveral frequencies/notes saved in a char that is playable with this function
+         */
         void rtttl_play(const char *s);
 
     };
