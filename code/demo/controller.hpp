1a593a4e370d0db16194293cd4e1689297d8be1a
PatrickDekker98 <patrick_dekker@hotmail.com>
2019-05-28 14:27:48 +0200

    changed travis somewhat so it will build forgot some doxygen so it was added

--- code/demo/controller.hpp
+++ code/demo/controller.hpp
@@ -1,15 +1,30 @@
+/// @file
+/// @brief module class for sound playback module
+/// @author Patrick Dekker
 #pragma once
 #include <base_module.hpp>
 #include <hwlib.hpp>
 
 namespace r2d2::demo {
+    /**
+     * @brief demo controller class for demo
+     */
     class controller_c : public base_module_c {
     private:
         hwlib::pin_in &pin;
         const char c[155] = "H: d=4,o=5,b=108: 2a4, 2e, 2d#, 2b4, 2a4, 2c, 2d, 2a#4, 2e., e, 1f4, 1a4, 1d#, 2e., d, 2c., b4, 1a4, 1p, 2a4, 2e, 2d#, 2b4, 2a4, 2c, 2d, 2a#4, 2e., e, 1f4";
     public:
+        /**
+         * @brief constructor for controller c
+         * @param comm
+         * @param pin , pin_in for button
+         */
         controller_c(base_comm_c & comm, hwlib::pin_in & pin)
             : base_module_c(comm), pin(pin) {}
+        /**
+         * @brief processes the code
+         * @details if button is pressed it sends an rtttl string
+         */
         void process() override {
             while (true) {
                 if (!pin.read()) {
--- code/headers/sound.hpp
+++ code/headers/sound.hpp
@@ -8,13 +8,22 @@
 
 namespace r2d2::sound {
 
-class sound_c : public r2d2::base_module_c {
-protected:
-    rtttl_player_c &player;
-public:
-     sound_c(base_comm_c & comm, rtttl_player_c & player);
+    /**
+     * @brief sound base module for sound playback
+     *
+     */
+    class sound_c : public r2d2::base_module_c {
+    protected:
+        rtttl_player_c &player;
+    public:
+        /**
+         * @brief constructor for sound_c
+         * @param comm
+         * @param player , a rtttl_player
+         */
+        sound_c(base_comm_c & comm, rtttl_player_c & player);
 
-     void process() override;
+        void process() override;
 
 };
 
